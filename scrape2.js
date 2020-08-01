df6f7c7baf885fbe6781846c29e174a6de6c4d21
Yaroslav <doctor355@yahoo.com>
2020-08-01 15:07:28 +0300

    get airports name from 1 page and pag-1

--- scrape2.js
+++ scrape2.js
@@ -5,7 +5,11 @@ request(
   "https://www.world-airport-codes.com/alphabetical/airport-code/a.html?page=1/",
   (error, response, html) => {
     if (!error && response.statusCode == 200) {
-     
+      const $ = cheerio.load(html);
+     $('.stack2 tbody tr').each((i, el)=>{
+       const title = $(el).find('th a').text()
+       console.log(title)
+     })
     } else {
       console.log(error);
     }
